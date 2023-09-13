3a72d3de2fbcd9b59d4204c8dd4b143f12b909ae
Mercy Mugambi <mercymugambi2016@gmail.com>
2023-09-13 16:44:05 +0300

    Add code to execute darktheme on the ui

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { ThemeProvider, createTheme, Box } from '@mui/material';
+import { useState } from 'react';
+import Header from './components/Header';
+
 function App() {
+  const [mode] = useState('light');
+
+  const darkTheme = createTheme({
+    palette: {
+      mode,
+    },
+  });
   return (
-    <div className="App">
-      <h2>hello!</h2>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <Box bgcolor="background.default" color="text.primary">
+        <Header />
+      </Box>
+    </ThemeProvider>
   );
 }
 
