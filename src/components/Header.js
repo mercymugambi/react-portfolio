2912e6c6cf9c77ab0bc8023fb61f20e86caf3fa5
Mercy Mugambi <mercymugambi2016@gmail.com>
2023-09-14 10:17:26 +0300

    Add projects lists using material UI

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
-  AppBar, Toolbar, Typography, Tabs, Tab,
+  AppBar, Toolbar, Typography, Tabs, Tab, useMediaQuery, useTheme,
 } from '@mui/material';
-import CodeIcon from '@mui/icons-material/Code';
 import MaterialUISwitch from './Switch';
+import DrawerComp from './DrawerComp';
 
-const Header = () => (
-  <>
-    <AppBar>
-      <Toolbar>
-        <Typography>
-          <CodeIcon />
-          MercyMugambi
-        </Typography>
-        <Tabs textColor="inherit">
-          <Tab label="Home" />
-          <Tab label="Projects" />
-          <Tab label="About" />
-          <Tab label="Contact Me" />
-        </Tabs>
-        <MaterialUISwitch />
-      </Toolbar>
-    </AppBar>
-  </>
-);
+const Header = () => {
+  const [value, setValue] = useState(0);
+  const theme = useTheme();
+  const isMatch = useMediaQuery(theme.breakpoints.down('sm'));
+
+  return (
+    <>
+      <AppBar>
+        <Toolbar>
+          <Typography>
+            Mercy
+          </Typography>
+          { isMatch ? (
+            <>
+              <DrawerComp />
+            </>
+          ) : (
+            <>
+              <Tabs
+                textColor="inherit"
+                sx={{ marginLeft: 'auto' }}
+                value={value}
+                onChange={(e, newValue) => setValue(newValue)}
+                indicatorColor="secondary"
+              >
+                <Tab label="Projects" />
+                <Tab label="About" />
+                <Tab label="Contact Me" />
+              </Tabs>
+            </>
+          )}
+          <MaterialUISwitch />
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
 
 export default Header;
