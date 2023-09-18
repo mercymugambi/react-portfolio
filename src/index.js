409a93e0568b6aedd6734d979ef2afe307459308
Mercy Mugambi <mercymugambi2016@gmail.com>
2023-09-18 20:52:33 +0300

    Include projects stylesheet on index

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './assets/css/style.css';
+import './assets/css/projects.css';
+
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
